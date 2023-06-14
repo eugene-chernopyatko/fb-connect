0fa9ea17ebad8810e1644739b0bed9577f611cc5
Eugene Chernopyatko <geka2678@gmail.com>
2023-06-14 21:19:40 +0300

    add allowed hostst

--- fbcost/settings.py
+++ fbcost/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z+(1@_2ht&4y#u0819wlg_w-ai^hn_am-l(0!qc52w%!7p@%wv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['neyokee.pythonanywhere.com']
 AUTH_USER_MODEL = 'authentication.CustomUser'
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
