b922ee68c3fa323779c83b3648c2620a99631e50
Eugene Chernopyatko <geka2678@gmail.com>
2023-06-20 02:01:37 +0300

    editing html and buttons

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.user_login, name='home'),
     path('logout/', views.user_logout, name='logout'),
     path('registration/', views.user_registration),
-    path('raccount/', views.reg_permission, name='perm')
+    path('account/', views.reg_permission, name='perm')
 ]
